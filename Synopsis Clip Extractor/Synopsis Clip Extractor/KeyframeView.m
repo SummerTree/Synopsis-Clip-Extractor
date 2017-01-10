3eb618c339c5a57cc62da9ce15ee3db9a4462130
vade <vade@vade.info>
2017-01-10 15:04:30 -0500

    Math is hard

--- Synopsis Clip Extractor/Synopsis Clip Extractor/KeyframeView.m
+++ Synopsis Clip Extractor/Synopsis Clip Extractor/KeyframeView.m
@@ -16,11 +16,51 @@
 @property (nonatomic, assign) CGFloat magnification;
 
 // Scaled units
-@property (nonatomic, assign) NSUInteger frameTickInPixels;
-@property (nonatomic, assign) NSUInteger frameDurationInPixels;
-@property (nonatomic, assign) NSUInteger totalDurationInPixels;
+@property (nonatomic, assign) CGFloat frameTickInPixels;
+@property (nonatomic, assign) CGFloat frameDurationInPixels;
+@property (nonatomic, assign) CGFloat totalDurationInPixels;
 
 @end
+
+static inline double Mod(double x, double y)
+{
+    if (0. == y)
+        return x;
+    
+    double m= x - y * floor(x/y);
+    
+    // handle boundary cases resulted from floating-point cut off:
+    
+    if (y > 0)              // modulo range: [0..y)
+    {
+        if (m>=y)           // Mod(-1e-16             , 360.    ): m= 360.
+            return 0;
+        
+        if (m<0 )
+        {
+            if (y+m == y)
+                return 0  ; // just in case...
+            else
+                return y+m; // Mod(106.81415022205296 , _TWO_PI ): m= -1.421e-14
+        }
+    }
+    else                    // modulo range: (y..0]
+    {
+        if (m<=y)           // Mod(1e-16              , -360.   ): m= -360.
+            return 0;
+        
+        if (m>0 )
+        {
+            if (y+m == y)
+                return 0  ; // just in case...
+            else
+                return y+m; // Mod(-106.81415022205296, -_TWO_PI): m= 1.421e-14
+        }
+    }
+    
+    return m;
+}
+
 
 @implementation KeyframeView
 
@@ -107,29 +147,33 @@ const double factor = 10.0;
     }
 }
 
-- (void) drawTickNumber:(NSUInteger)i withTransform:(CGAffineTransform)transform withColor:(NSColor*)color atTickScale:(NSUInteger)scale height:(CGFloat)height inContext:(CGContextRef)context maxTickMag:(float)tickMag label:(NSString*)label maxLabelMag:(float)labelMag
+- (void) drawAtPoint:(CGPoint)x withColor:(NSColor*)color atTickScale:(CGFloat)scale height:(CGFloat)height inContext:(CGContextRef)context maxTickMag:(float)tickMag label:(NSString*)label maxLabelMag:(float)labelMag
 {
     if(self.magnification >= tickMag)
     {
         // Draw Frame Boundaries
         [color setStroke];
         [color setFill];
-        if( i % scale == 0)
+        
+//        if( i % scale == 0)
+        
+        float draw = Mod(x.x, scale);
+        
+        if( draw/self.magnification <=  FLT_EPSILON)
         {
-            CGPoint x = CGPointMake(i, 0);
+//            CGPoint x = CGPointMake((CGFloat)i, 0);
+//
+//            x = CGPointApplyAffineTransform(x, transform);
 
-            x = CGPointApplyAffineTransform(x, transform);
-
-//            CGFloat x = i - fmod(self.scrollOrigin, scale);            
-//            if(x.x <= self.totalDurationInPixels)
+            if(x.x <= self.totalDurationInPixels)
             {
                 NSPoint p1 = NSMakePoint(x.x, self.bounds.size.height);
                 NSPoint p2 = NSMakePoint(x.x, self.bounds.size.height * height);
                 
                 [NSBezierPath strokeLineFromPoint:p1 toPoint:p2];
                 
-                if(label)
-                    [self drawLabel:[NSString stringWithFormat:label, i/scale] atPoint:p2 atTickScale:scale inContext:context magnification:labelMag];
+//                if(label)
+//                    [self drawLabel:[NSString stringWithFormat:label, i/scale] atPoint:p2 atTickScale:scale inContext:context magnification:labelMag];
             }
         }
     }
@@ -152,38 +196,78 @@ const double factor = 10.0;
     NSUInteger minute = (second * 60);
     NSUInteger hour = (minute * 60);
     
-    CGAffineTransform transform = CGAffineTransformIdentity;
+//    CGFloat scrollAmount = fmod(self.scrollOrigin, self.bounds.size.width);
     
-    transform = CGAffineTransformTranslate(transform, -self.scrollOrigin, 0.0);
-    transform = CGAffineTransformScale(transform, self.magnification, 1.0);
+    CGAffineTransform scrollTransform = CGAffineTransformMakeTranslation(-self.scrollOrigin, 0.0);
+    CGAffineTransform invScrollTransform = CGAffineTransformMakeTranslation(-self.scrollOrigin, 0.0);
+
+    CGAffineTransform magTransform = CGAffineTransformMakeScale(self.magnification, 1.0);
+    CGAffineTransform invMagTransform = CGAffineTransformMakeScale(1.0/self.magnification, 1.0);
+
+    CGAffineTransform transform = CGAffineTransformConcat(scrollTransform, magTransform );
+    CGAffineTransform invTransform = CGAffineTransformConcat(invScrollTransform, invMagTransform);
+    
+    CGRect totalTimeline = CGRectMake(0, 0, self.totalDurationInPixels, self.bounds.size.height);
+    CGRect transformedTimeline = CGRectApplyAffineTransform(totalTimeline, transform);
+    CGRect invTransformedTimeline = CGRectApplyAffineTransform(totalTimeline, invMagTransform);
     
     for(NSInteger i = 0; i < count; i++)
     {
         NSRect clippedDirtyRect = rects[i];
+        
+        // Rect that indicates our scrolled coordinate system in 1:1 coordinates
+        CGRect scrolledClippedRect = CGRectApplyAffineTransform(clippedDirtyRect, invTransform);
 
+        // Intersect our 2 rects, giving us a window into our transformed rect that has the correct origin for our virtual scroller
+        CGRect windowedClippedRect = CGRectIntersection(scrolledClippedRect, transformedTimeline);
+        
         [[NSColor darkGrayColor] setFill];
         CGContextFillRect(context, clippedDirtyRect);
         
-        NSRect transformedDirtyRect = (NSRect)CGRectApplyAffineTransform((CGRect)clippedDirtyRect, transform);
-        
-        NSLog(@"transformedDirtyRect %@", NSStringFromRect(transformedDirtyRect));
-        
-        for(NSUInteger i = 0; i < clippedDirtyRect.size.width; i ++)
+//        clippedDirtyRect = (NSRect)CGRectApplyAffineTransform((CGRect)clippedDirtyRect, CGAffineTransformMakeTranslation(scrollAmount, 0));
+//        NSLog(@"true bounds %@", NSStringFromRect(self.bounds));
+//        NSLog(@"transformedTimeline %@", NSStringFromRect(transformedTimeline));
+//        NSLog(@"invtransformedTimeline %@", NSStringFromRect(invTransformedTimeline));
+//        NSLog(@"windowedClippedRect %@", NSStringFromRect(windowedClippedRect));
+
+        [[NSColor whiteColor] setFill];
+        [[NSColor whiteColor] setStroke];
+
+//        use origin of transformed rect to offset scroll position or something?
+        for(CGFloat i = 0; i < clippedDirtyRect.size.width; i+=1.0)
         {
-            // Draw Tick
-            [self drawTickNumber:i withTransform:transform withColor:[NSColor blackColor] atTickScale:self.frameTickInPixels height:0.9 inContext:context maxTickMag:2.0 label:nil maxLabelMag:0];
+            CGPoint x = CGPointMake(i, 0);
+            
+            
+            // Test Code
+            float draw = fmod(i, self.frameDurationInPixels);
+            
+            if( draw <= FLT_EPSILON)
+            {
+                CGPoint top = CGPointMake(i, transformedTimeline.size.height);
+                
+                top = CGPointApplyAffineTransform(top, transform);
+//                top.x = Mod(top.x , scrolledClippedRect.size.width );
 
-            // Draw Frame
-            [self drawTickNumber:i withTransform:transform  withColor:[NSColor lightGrayColor] atTickScale:self.frameDurationInPixels height:0.8 inContext:context maxTickMag:0.25 label:@"f %i" maxLabelMag:1.5];
-
-            // Draw Seconds
-            [self drawTickNumber:i withTransform:transform  withColor:[NSColor yellowColor] atTickScale:second height:0.7 inContext:context maxTickMag:0.01 label:@"s %i" maxLabelMag:0.05];
-
-            // Draw Minutes
-            [self drawTickNumber:i withTransform:transform  withColor:[NSColor redColor] atTickScale:minute height:0.6 inContext:context maxTickMag:0 label:@"m %i" maxLabelMag:0.0];
-
-            // Draw Hour
-            [self drawTickNumber:i withTransform:transform  withColor:[NSColor greenColor] atTickScale:hour height:0.5 inContext:context maxTickMag:0 label:@"h %i" maxLabelMag:0.0];
+                CGPoint bottom = CGPointMake(top.x, 0);
+                
+                [NSBezierPath strokeLineFromPoint:top toPoint:bottom];
+            }
+            
+//            // Draw Tick
+//            [self drawAtPoint:x withColor:[NSColor blackColor] atTickScale:scaledFrameTickInPixels.x height:0.9 inContext:context maxTickMag:2.0 label:nil maxLabelMag:0];
+//
+//            // Draw Frame
+//            [self drawAtPoint:x withColor:[NSColor lightGrayColor] atTickScale:scaledFrameDurationInPixels.x height:0.8 inContext:context maxTickMag:0.25 label:@"f %f" maxLabelMag:1.5];
+//
+//            // Draw Seconds
+//            [self drawAtPoint:x withColor:[NSColor yellowColor] atTickScale:scaledSecond.x height:0.7 inContext:context maxTickMag:0.01 label:@"s %f" maxLabelMag:0.05];
+//
+//            // Draw Minutes
+//            [self drawAtPoint:x withColor:[NSColor redColor] atTickScale:scaledMinute.x height:0.6 inContext:context maxTickMag:0 label:@"m %f" maxLabelMag:0.0];
+//
+//            // Draw Hour
+//            [self drawAtPoint:x withColor:[NSColor greenColor] atTickScale:scaledHour.x height:0.5 inContext:context maxTickMag:0 label:@"h %f" maxLabelMag:0.0];
         }
     }
 }
--- Synopsis Clip Extractor/Synopsis Clip Extractor/TimelineView.m
+++ Synopsis Clip Extractor/Synopsis Clip Extractor/TimelineView.m
@@ -10,15 +10,61 @@
 @interface TimelineView()
 @property (nonatomic, assign) CMTime frameDuration;
 @property (nonatomic, assign) CMTime duration;
+@property (nonatomic, assign) NSUInteger numFrames;
+
+@property (nonatomic, assign) CGFloat scrollOrigin;
+@property (nonatomic, assign) CGFloat magnification;
+
+@property (nonatomic, assign) CGFloat zoomRangeMin;
+@property (nonatomic, assign) CGFloat zoomRangeMax;
+
+
 @end
+
+
+//check for division by zero???
+//--------------------------------------------------
+static inline CGFloat map(CGFloat value, CGFloat inputMin, CGFloat inputMax, CGFloat outputMin, CGFloat outputMax, bool clamp)
+{
+    if (fabs(inputMin - inputMax) < FLT_EPSILON)
+    {
+        return outputMin;
+    }
+    else
+    {
+        float outVal = ((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin);
+        
+        if( clamp )
+        {
+            if(outputMax < outputMin)
+            {
+                if( outVal < outputMax )outVal = outputMax;
+                else if( outVal > outputMin )outVal = outputMin;
+            }
+            else
+            {
+                if( outVal > outputMax )outVal = outputMax;
+                else if( outVal < outputMin )outVal = outputMin;
+            }
+        }
+        return outVal;
+    }
+}
+
+
 
 @implementation TimelineView
 
 - (void) awakeFromNib
 {
-    self.enclosingScrollView.hasVerticalScroller = NO;
-    self.enclosingScrollView.hasHorizontalScroller = YES;
-    self.enclosingScrollView.scrollsDynamically = YES;
+    self.duration = CMTimeMakeWithSeconds(60 * 5, 600);
+    self.frameDuration = CMTimeMake(1,30);
+    self.magnification = 1.0;
+    
+    self.zoomRangeMax = 1.0;//self.bounds.size.width;
+    self.zoomRangeMin = 0.0;
+    
+    [self recalculateUnits];
 }
 
 - (void) setFrameFromDuration:(CMTime)duration andFrameDuration:(CMTime)frameDuration
@@ -28,8 +74,6 @@
         self.frameDuration = frameDuration;
         self.duration = duration;
         
-        [self setBoundsSize:NSMakeSize(self.duration.value / self.frameDuration.value, self.bounds.size.height)];
-        NSLog(@"size : %@", NSStringFromSize(self.bounds.size));
         [self setNeedsDisplay:YES];
     }
 }
@@ -38,6 +82,71 @@
 {
     return YES;
 }
+
+- (void) magnifyWithEvent:(NSEvent *)event
+{
+    self.magnification += event.magnification;
+    self.magnification = MAX(0.001, self.magnification);
+    self.magnification = MIN(10, self.magnification);
+    
+    //    NSPoint pointInView = [self convertPoint:event.locationInWindow fromView:nil];
+    //    NSLog(@"pointInView: %f", pointInView.x );
+    //    [self updateScrollOrigin: ( -pointInView.x )];
+    
+    [self recalculateUnits];
+    
+    //    NSSize scale = NSMakeSize(self.scale.width + event.magnification, 1.0);
+    //    [self setScale:scale];
+}
+
+- (void) scrollWheel:(NSEvent *)event
+{
+//    if(self.bounds.size.width < self.totalDurationInPixels)
+    {
+        self.scrollOrigin -= event.deltaX / self.bounds.size.width;
+        self.scrollOrigin = MAX(self.scrollOrigin, 0);
+        //    self.scrollOrigin = MIN(self.scrollOrigin, (self.totalDurationInFrames * self.magnification) - self.bounds.size.width);
+        [self recalculateUnits];
+    }
+}
+
+- (void) updateScrollOrigin:(CGFloat)amount
+{
+    
+}
+- (void) recalculateUnits
+{
+    self.numFrames = ceil(CMTimeGetSeconds(self.duration) / CMTimeGetSeconds(self.frameDuration));
+    
+    self.zoomRangeMin = 0.0 + self.scrollOrigin;
+    self.zoomRangeMax = self.magnification + self.scrollOrigin;;
+    
+//    self.durationInFrames = self.frameDuration.value * self.duration.value;
+//    self.frameTickInPixels = (1.0);
+//    self.frameDurationInPixels = (self.frameTickInPixels * self.frameDuration.value );
+//    self.totalDurationInPixels = (self.frameTickInPixels * self.duration.value);
+//    
+//    NSLog(@"m %f ftp %lu, fdp %lu, tdp %lu", self.magnification, (unsigned long)self.frameTickInPixels, (unsigned long)self.frameDurationInPixels, (unsigned long)self.totalDurationInPixels);
+    
+    [self setNeedsDisplay:YES];
+}
+
+
+- (CGFloat) millisToScreenX:(CGFloat)millis
+{
+    return [self normalizedXtoScreenX:(millis/(CMTimeGetSeconds(self.duration) * 1000.0))];
+}
+
+- (float) normalizedXtoScreenX:(float)x
+{
+//    return normalizedXtoScreenX(x, getViewRange());
+    return map(x, self.zoomRangeMin, self.zoomRangeMax, self.bounds.origin.x, self.bounds.size.width, false);
+}
+
+//float normalizedXtoScreenX(float x, ofRange inputRange){
+//    return ofMap(x, inputRange.min, inputRange.max, getDrawRect().getMinX(), getDrawRect().getMaxX(), false);
+//}
+
 
 - (void)drawRect:(NSRect)dirtyRect {
     
@@ -61,20 +170,54 @@
         [[NSColor darkGrayColor] setFill];
         CGContextFillRect(context, clippedDirtyRect);
         
-        for(NSUInteger i = 0; i < clippedDirtyRect.size.width ; i ++)
+        // Draw Frames
+        for(int i = 0; i < self.numFrames; i++)
         {
-            CGFloat x = (CGFloat)i + clippedDirtyRect.origin.x;
-            
-            // Draw tick Boundaries
-            [[NSColor blackColor] setStroke];
-            if( i % 2 == 0)
+//            CGFloat frame = i
+            float screenX = [self millisToScreenX:(i * CMTimeGetSeconds(self.frameDuration) * 1000.0)];
+            if(screenX > clippedDirtyRect.origin.x && screenX < clippedDirtyRect.size.width)
             {
-                NSPoint p1 = NSMakePoint(x, clippedDirtyRect.size.height);
-                NSPoint p2 = NSMakePoint(x, clippedDirtyRect.size.height * 0.5);
+                [[NSColor lightGrayColor] setFill];
+                [[NSColor lightGrayColor] setStroke];
+
+                CGPoint top = CGPointMake(screenX, clippedDirtyRect.size.height);
+                CGPoint bottom = CGPointMake(top.x, clippedDirtyRect.size.height * 0.9);
                 
-                [NSBezierPath strokeLineFromPoint:p1 toPoint:p2];
+                [NSBezierPath strokeLineFromPoint:top toPoint:bottom];
             }
         }
+        
+        for(int i = 0; i < floor(CMTimeGetSeconds(self.duration)); i++)
+        {
+            float screenX = [self millisToScreenX:(i * 1000.0)];
+            if(screenX > clippedDirtyRect.origin.x && screenX < clippedDirtyRect.size.width)
+            {
+                [[NSColor yellowColor] setFill];
+                [[NSColor yellowColor] setStroke];
+                
+                CGPoint top = CGPointMake(screenX, clippedDirtyRect.size.height);
+                CGPoint bottom = CGPointMake(top.x, clippedDirtyRect.size.height * 0.8);
+                
+                [NSBezierPath strokeLineFromPoint:top toPoint:bottom];
+            }
+        }
+        
+        for(int i = 0; i < floor(CMTimeGetSeconds(self.duration)/60.0); i++)
+        {
+            float screenX = [self millisToScreenX:(i * 1000.0 * 60)];
+            if(screenX > clippedDirtyRect.origin.x && screenX < clippedDirtyRect.size.width)
+            {
+                [[NSColor orangeColor] setFill];
+                [[NSColor orangeColor] setStroke];
+                
+                CGPoint top = CGPointMake(screenX, clippedDirtyRect.size.height);
+                CGPoint bottom = CGPointMake(top.x, clippedDirtyRect.size.height * 0.7);
+                
+                [NSBezierPath strokeLineFromPoint:top toPoint:bottom];
+            }
+        }
+
+        
     }
 }
 @end
